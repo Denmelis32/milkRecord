7bf483e50d50f6634a74e9fddbc1e840fde9902e
El_Mitya32 <el_mitya@mail.ru>
2019-07-27 12:12:54 +0300

    first commit

--- milkRecordd/app/src/main/java/com/bignerdranch/android/milkrecord/MainActivity.java
+++ milkRecordd/app/src/main/java/com/bignerdranch/android/milkrecord/MainActivity.java
@@ -1,13 +1,67 @@
 package com.bignerdranch.android.milkrecord;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText number;
+    TextView operation;
+    Double operand = null;
+    String lastOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result =(TextView) findViewById(R.id.resultField);
+        number = (EditText) findViewById(R.id.numberField);
+        operation = (TextView) findViewById(R.id.operationField);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void Operation(Double number, String operation){
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        result.setText(operand.toString().replace('.', ','));
+        number.setText("");
     }
 }
